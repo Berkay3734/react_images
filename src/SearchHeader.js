2071ad7b97c273a2c80949e033c22361e50d15fe
Berkay Gençdoğan <berkay.gameofbilisim@gmail.com>
2023-07-02 12:39:09 +0300

    finished

--- src/SearchHeader.js
+++ src/SearchHeader.js
@@ -1,9 +1,22 @@
-function SearchHeader() {
+import { useState } from 'react';
+
+function SearchHeader({ search }) {
+  const [valueInput, setValue] = useState('');
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    debugger;
+    search(valueInput);
+  };
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <div className="searchDiv">
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <label>Ne Arıyorsunuz?</label>
-        <input />
+        <input value={valueInput} onChange={handleChange} />
       </form>
     </div>
   );
